9a8da9b33099f3124ce76adb914d4a85962390ce
michelle-currier <mushel@gmail.com>
2024-12-01 11:23:41 -0600

    putting things back

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,25 @@
-// import path from "path";
-// import { fileURLToPath } from "url";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src"); // Ensure it's pointing to 'src'
+    return config;
+  },
+  // experimental: {
+  //   appDir: true, // Explicitly enable app directory
+  // },
+
+  // output: "export", // remove when working locally add back to build the static deploy
+};
+
 // const nextConfig = {
-//   webpack: (config) => {
-//     config.resolve.alias["@"] = path.resolve(__dirname, "src"); // Ensure it's pointing to 'src'
-//     return config;
-//   },
 //   experimental: {
 //     appDir: true, // Explicitly enable app directory
 //   },
-
-//   // output: "export", // remove when working locally add back to build the static deploy
 // };
-
-const nextConfig = {
-  experimental: {
-    appDir: true, // Explicitly enable app directory
-  },
-};
 export default nextConfig;
